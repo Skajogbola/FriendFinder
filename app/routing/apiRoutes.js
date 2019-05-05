d490ee9b02b7ff5e180948d4024f0b35a7d04ab8
Simmie <skajogbola@users.noreply.github.com>
2019-05-05 09:42:47 -0400

    commit

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,26 +1,46 @@
-var tableData = require("../data/tableData");
+var friends = require("../data/friends");
 
 module.exports = function(app) {
-
   app.get("/api/friends", function(req, res) {
-    res.json(tableData);
+    res.json(friends);
   });
 
 
   app.post("/api/fiends", function(req, res) {
-    if (tableData.length < 5) {
-      tableData.push(req.body);
-      res.json(true);
-    }
-    else {
-      waitListData.push(req.body);
-      res.json(false);
-    }
-  });
+      var userInput = req.body;
+    
+      for(var i = 0; i < userInput.scores.length; i++) {
+        userInput.scores[i] = parseInt(userInput.scores[i]);
+      }
+  
+      // default friend match is the first friend but result will be whoever has the minimum difference in scores
+      var bestFriendIndex = 0;
+      var minimumDifference = 40;
+      //  whatever the difference is, add to the total difference
+    for(var i = 0; i < friends.length; i++) {
+        var totalDifference = 0;
+        for(var j = 0; j < friends[i].scores.length; j++) {
+          var difference = Math.abs(userInput.scores[j] - friends[i].scores[j]);
+          totalDifference += difference;
+        }
+  
+        // if there is a new minimum, change the best friend index and set the new minimum for next iteration comparisons
+        if(totalDifference < minimumDifference) {
+          bestFriendIndex = i;
+          minimumDifference = totalDifference;
+        }
+      }
+  
+      // after finding match, add user to friend array
+      friends.push(user);
+  
+      // send back to browser the best friend match
+      res.json(friends[bestFriendIndex]);
+    });
 
 
   app.post("/api/clear", function(req, res) {
-    tableData.length = 0;
+    friends.length = 0;
     res.json({ ok: true });
   });
 };
