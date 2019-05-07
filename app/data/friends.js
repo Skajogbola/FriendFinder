d42b461adc1fabb1442925c8c4bf5af58baec241
Simmie <skajogbola@users.noreply.github.com>
2019-05-07 03:45:00 -0400

    commit

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,5 +1,52 @@
-var friends= [
-   
+var friends = [
+    {
+        name: "Tom Harry",
+        photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/0/0a/Charlie_%289%29.jpg",
+        scores: [
+            "4",
+            "3",
+            "2",
+            "1",
+            "4",
+            "4",
+            "4",
+            "4",
+            "5",
+            "5",
+        ]
+    },
+    {
+        name: "Diane Ross",
+        photo: "https://images.pexels.com/photos/556667/pexels-photo-556667.jpeg",
+        scores: [
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "4",
+            "4",
+            "4",
+            "5",
+            "5",
+        ]
+    },
+    {
+        name: "Becky Lou",
+        photo: "http://www.sarkarinaukrisearch.in/wp-content/uploads/2018/12/south-actress-images-7.jpg",
+        scores: [
+            "3",
+            "3",
+            "2",
+            "1",
+            "4",
+            "5",
+            "1",
+            "4",
+            "4",
+            "3",
+        ]
+    },
 ];
 
 module.exports = friends;
